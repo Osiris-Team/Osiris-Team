f0695531acca7dfa3253a2e7a73c940ce1f99d56
Osiris-Team <59899645+Osiris-Team@users.noreply.github.com>
2023-08-22 14:43:26 +0200

    Update update_issue_comment.py

--- update_issue_comment.py
+++ update_issue_comment.py
@@ -15,15 +15,24 @@ session_end = "<!--WORK_SESSIONS_END-->"
 total_start = "<!--WORK_SESSIONS_TOTAL_START-->"
 total_end = "<!--WORK_SESSIONS_TOTAL_END-->"
 
+print("Issue Body:")
+print(issue_body)
+
 sessions_match = re.search(f"{session_start}(.*?){session_end}", issue_body, re.DOTALL)
 if sessions_match:
     sessions_content = sessions_match.group(1)
     total_minutes = sum(map(int, re.findall(r"Took.*\((\d+)\s*min\)", sessions_content)))
 
+    print("Sessions Content:")
+    print(sessions_content)
+
     updated_sessions_content = f"{session_start}\n{sessions_content.strip()}\n{session_end}"
     updated_total_content = f"{total_start}\nTotal work time: {total_minutes} min\n{total_end}"
 
     updated_issue_body = re.sub(f"{session_start}(.*?){total_end}", f"{updated_sessions_content}\n\n{updated_total_content}", issue_body, re.DOTALL)
+
+    print("Updated Issue Body:")
+    print(updated_issue_body)
 
     issue.edit(body=updated_issue_body)
 else:
